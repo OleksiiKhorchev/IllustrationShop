c7ada4799c9972de78bd9f3e0afde4ab99e69047
Oleksii Khorchev <Oleksii.Khorchev@gmail.com>
2015-02-22 11:40:22 +0200

    Added a test WCF service

---
+++ IllustrationShop.Web/Services/ExampleService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IllustrationShop.Web.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ExampleService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ExampleService.svc or ExampleService.svc.cs at the Solution Explorer and start debugging.
+    public class ExampleService : IExampleService
+    {
+        public string GetExample(int exampleId)
+        {
+            return "Here is your Example: " + exampleId;
+        }
+    }
+}
